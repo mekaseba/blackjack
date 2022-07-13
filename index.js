52f5b4ca0e481f3f25de761e72de45affd0d9b1a
Meka Seba <mekasebameka@gmail.com>
2022-07-12 17:56:00 -0700

    sum progress

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-let firstCard = 11
-let secondCard = 10
-let sum = firstCard + secondCard + 3
+let firstCard = 10
+let secondCard = 2
+let sum = firstCard + secondCard 
+let hasBlackJack = false 
+let isAlive = true 
+let message = ""
+let msgEl = document.getElementById("msg-el")
+let sumEl = document.getElementById("sum-el")
+let cardEl = document.getElementById("card-el")
 
-if (sum < 21) {
-    console.log("Do you wanna draw a new card? 🙂")
-} else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
-} else {
-    console.log("You're outta the game. 😭")
+function start() {
+    sumEl.innerText = "Sum: " + sum
+    cardEl.innerText = "Cards: " + firstCard + " & " + secondCard
+    if (sum <= 20) {
+        message = "Do you wanna draw a new card?"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're outta the game!"
+        isAlive = false 
+    }   
+    msgEl.innerText = message
+}
+
+function draw() {
+    let newCard = 2
+    sum += newCard
+    start()
 }
